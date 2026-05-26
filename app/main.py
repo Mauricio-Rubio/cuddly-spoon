34b85bc3aebfafe2eb32fc2ef01c3822f4d3c9e8
Mauricio Rubio <mau.r.h54@gmail.com>
2026-05-26 22:06:18 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ BASE_URL = os.getenv("OPENROUTER_BASE_URL", default="https://openrouter.ai/api/v
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", required=True)
-    parser.add_argument("filename", nargs='?')
+    parser.add_argument("filename", nargs="?")
     args = parser.parse_args()
 
     messages = [{"role": "user", "content": args.p}]
@@ -31,47 +31,51 @@ def main():
 
     TOOL_MAPPING = {"read": read_file}
 
-    chat = client.chat.completions.create(
-        model="anthropic/claude-haiku-4.5",
-        messages=messages,
-        tools=[
-            {
-                "type": "function",
-                "function": {
-                    "name": "read",
-                    "description": "Read and return the contents of a file",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "file_path": {
-                                "type": "string",
-                                "description": "The path to the file to read",
-                            }
+    while True:
+        chat = client.chat.completions.create(
+            model="anthropic/claude-haiku-4.5",
+            messages=messages,
+            tools=[
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "read",
+                        "description": "Read and return the contents of a file",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "file_path": {
+                                    "type": "string",
+                                    "description": "The path to the file to read",
+                                }
+                            },
+                            "required": ["file_path"],
                         },
-                        "required": ["file_path"],
                     },
-                },
-            }
-        ],
-    )
+                }
+            ],
+        )
 
-    if not chat.choices or len(chat.choices) == 0:
-        raise RuntimeError("no choices in response")
+        if not chat.choices or len(chat.choices) == 0:
+            raise RuntimeError("no choices in response")
+        response = chat.choices[0].message
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!", file=sys.stderr)
+        if response.tool_calls is None:
+            print(response.content)
+            break
+        # You can use print statements as follows for debugging, they'll be visible when running tests.
+        # print("Logs from your program will appear here!", file=sys.stderr)
 
-    # TODO: Uncomment the following line to pass the first stage
-    response = False
-    for tool in chat.choices[0].message.tool_calls or []:
-        tool_name = tool.function.name
-        tool_args = json.loads(tool.function.arguments)
-        tool_response = TOOL_MAPPING[tool_name](**tool_args)
-        response = tool_response
-    if not response:
-        response = chat.choices[0].message.content
-
-    print(response)
+        # TODO: Uncomment the following line to pass the first stage
+        for tool in response.tool_calls or []:
+            tool_name = tool.function.name
+            tool_args = json.loads(tool.function.arguments)
+            tool_response = TOOL_MAPPING[tool_name](**tool_args)
+            messages.append({
+                "role": "tool",
+                "tool_call_id": tool.id,
+                "content": tool_response,
+            })
 
 
 if __name__ == "__main__":
